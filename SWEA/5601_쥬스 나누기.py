f9a9f722b2d9678a43274044e84e9208cf32765a
smingi <87458297+smingi@users.noreply.github.com>
2022-03-30 21:17:33 +0900

    Add files via upload

---
+++ SWEA/5601_쥬스 나누기.py
@@ -0,0 +1,9 @@
+# swea 5601. 쥬스 나누기
+
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    print('#{}'.format(tc), end=' ')
+    for _ in range(N):
+        print('{}/{}'.format(1, N), end=' ')
+    print()
