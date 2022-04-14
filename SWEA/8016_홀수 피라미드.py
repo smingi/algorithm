133bbab441959fd2bf37d73eb93ce136261a9fc3
smingi <87458297+smingi@users.noreply.github.com>
2022-04-14 20:05:17 +0900

    Add files via upload

---
+++ SWEA/8016_홀수 피라미드.py
@@ -0,0 +1,8 @@
+# 8016. 홀수 피라미드
+
+t = int(input())
+for tc in range(1, t+1):
+    floor = int(input())
+    start = 2 * ((floor-1)**2 + 1 ) - 1
+    end = 2 * floor**2 - 1
+    print('#{} {} {}'.format(tc, start, end))
