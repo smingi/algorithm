96fbadcf796c6f4b57cba655ba9f36dbcd75fbcc
smingi <87458297+smingi@users.noreply.github.com>
2022-06-17 20:56:36 +0900

    Add files via upload

---
+++ 백준/1094_막대기.py
@@ -0,0 +1,20 @@
+# 백준 1094_막대기
+
+
+bar_lst = []
+number = 64
+while number > 0:
+    bar_lst.append(number)
+    number //= 2
+
+x = int(input())
+result = 0
+sumv = 0
+for num in bar_lst:
+    if sumv == x:
+        break
+    if sumv < x and sumv + num <= x:
+        sumv += num
+        result += 1
+
+print(result)
