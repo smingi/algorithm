ce6b00be968bd29891c1ad7d8cd57424fe0ae48c
smingi <87458297+smingi@users.noreply.github.com>
2022-06-24 22:10:47 +0900

    Update 12865_평범한 배낭.py

--- 백준/12865_평범한 배낭.py
+++ 백준/12865_평범한 배낭.py
@@ -9,8 +9,10 @@ for i in range(N):  # 물건 종류
     for j in range(1, K+1):  # 배낭 무게
         if j < lst[i][0]:  # 물건이 배낭 무게보다 무거우면
             dp[i][j] = dp[i-1][j]  # 넣기 전 상태 보전
+            
         elif dp[i-1][j] > dp[i-1][j-lst[i][0]]+lst[i][1]:  # 같은 무게에서 새로 넣은 물건 가치보다 기존 가치가 클 때
             dp[i][j] = dp[i-1][j]  # 기존 물건 유지
+            
         else:  # 같은 무게에서 새로 넣은 물건 가치가 클 때
             dp[i][j] = dp[i-1][j-lst[i][0]]+lst[i][1]  # 새로운 물건으로 교체
 
