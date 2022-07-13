bf08fea89d63a0840997f4da555a4904119bcc2a
smingi <87458297+smingi@users.noreply.github.com>
2022-07-13 13:25:11 +0900

    Add files via upload

---
+++ 백준/1987_소수 찾기.py
@@ -0,0 +1,22 @@
+# 백준 1987_소수 찾기
+
+def is_prime(num):
+    if num < 2:
+        return False
+
+    for i in range(2, num//2+1):
+        if num%i == 0:
+            return False
+
+    return True
+
+
+N = int(input())
+numbers = list(map(int, input().split()))
+
+cnt = 0
+for number in numbers:
+    if is_prime(number):
+        cnt += 1
+
+print(cnt)
